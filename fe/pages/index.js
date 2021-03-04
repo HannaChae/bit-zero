8767d0f910f839effccb45b8b543393069d77b13
HannaChae <hannachae.dev@gamil.com>
2021-03-04 15:00:06 +0900

    connect

--- fe/pages/index.js
+++ fe/pages/index.js
@@ -8,32 +8,12 @@ import { CountdownTimerOne } from "../components/Countdown";
 import blogData from "../data/blog-posts/blog-post-one.json";
 import heroSliderData from "../data/hero-sliders/hero-slider-seven.json";
 
-const Collection = () => {
+const Collection = () =>{
   return (
     <LayoutNine aboutOverlay={false}>
       {/* hero slider */}
       <HeroSliderSeven sliderData={heroSliderData} />
       <div className="space-mb--r100"></div>
-
-      {/* category grid */}
-      <SectionTitleOne
-        title="Spring summer 2020"
-        subtitle="Find your style. Fall fashion 20xx"
-      />
-      <CategoryGridThree spaceBottomClass="space-mb--r100" />
-
-      {/* countdown */}
-      <CountdownTimerOne
-        title="Deal of the day"
-        backgroundImage="./assets/images/countdown/bg-countdown-1.jpg"
-        dateTime="July 07, 2020 12:12:00"
-        url="/shop/left-sidebar"
-        buttonText="Only $39"
-        spaceBottomClass="space-mb--r100"
-      />
-
-      {/* blog post slider */}
-      <BlogPostSlider blogData={blogData} spaceBottomClass="space-mb--r100" />
     </LayoutNine>
   );
 };
